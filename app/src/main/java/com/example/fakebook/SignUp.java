2ae11c3801e855571334dca520e44e2f54d360fd
yuvalbarak8 <155367840+yuvalbarak8@users.noreply.github.com>
2024-02-16 01:03:55 +0200

    new design and fix the signup

--- app/src/main/java/com/example/fakebook/SignUp.java
+++ app/src/main/java/com/example/fakebook/SignUp.java
@@ -1,6 +1,9 @@
 package com.example.fakebook;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,12 +11,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,6 +27,7 @@ public class SignUp extends AppCompatActivity {
 
     private static final int PICK_IMAGE_REQUEST = 1;
     private static final int CAMERA_REQUEST = 2;
+    private Boolean imageTaken = Boolean.FALSE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +40,7 @@ public class SignUp extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateUsername();
-                validatePassword();
+                valid();
             }
         });
 
@@ -45,42 +51,104 @@ public class SignUp extends AppCompatActivity {
             }
         });
     }
-    private void validateUsername() {
-        EditText usernameEditText = findViewById(R.id.editTextTextPersonName);
-        TextView errorTextView = findViewById(R.id.textViewUsernameError); // Reusing the password error TextView for simplicity
+    @SuppressLint("SetTextI18n")
+    private void valid() {
+        // TextView for tell the user the status of signup
+        TextView message = findViewById(R.id.text_sign_up);
+        // get data from the user
+        EditText username_edit = findViewById(R.id.editTextTextPersonName);
+        EditText password_edit = findViewById(R.id.editTextTextPassword);
+        EditText password_again_edit = findViewById(R.id.editTextVerifyPassword);
+        EditText nickname_edit = findViewById(R.id.nickname);
+        // convert data to the type
+        String username = username_edit.getText().toString();
+        String password = password_edit.getText().toString();
+        String password_again = password_again_edit.getText().toString();
+        String nickname = nickname_edit.getText().toString();
 
-        String username = usernameEditText.getText().toString().trim();
-        Pattern pattern = Pattern.compile("^[a-zA-Z_][a-zA-Z0-9_]{3,19}$");
-        Matcher matcher = pattern.matcher(username);
-
-        if (username.isEmpty()) {
-            errorTextView.setText("Username cannot be empty");
-        } else if (!matcher.matches()) {
-            errorTextView.setText("Invalid username. Must have alphabet letters, and numbers");
-        } else {
-            errorTextView.setText("Good Username"); // Clear any previous error messages
+        // check if all fields are filled in
+        if(username.equals("") || password.equals("")|| password_again.equals("")||
+                nickname.equals(""))
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("All fields are required.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
         }
+        // Validity check for username
+        Pattern pattern = Pattern.compile("^[a-zA-Z0-9]{6,10}$");
+        Matcher matcher = pattern.matcher(username);
+        if(!matcher.matches())
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("Username must be 6-10 characters" +
+                    " without special characters.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
+        }
+        // Validity check for password
+        Pattern pattern1 = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,14}$");
+        Matcher matcher1 = pattern1.matcher(password);
+        if(!matcher1.matches())
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("Password must be 8-14 characters with at least one uppercase, one lowercase, and one number, without special characters.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
+        }
+        // Checking whether the two passwords are the same
+        if(!password.equals(password_again))
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("The passwords you typed are not the same.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
+        }
+        // Validity check for nickname
+        Pattern pattern2 = Pattern.compile("^[a-zA-Z\\d]{4,8}$");
+        Matcher matcher2 = pattern2.matcher(nickname);
+        if(!matcher2.matches())
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("Nickname must be 4-8 characters without special characters.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
+        }
+        // Validity check for nickname
+        if(!this.imageTaken)
+        {
+            message.setVisibility(View.VISIBLE);
+            message.setText("Please select a valid image file.");
+            message.setTextColor(android.graphics.Color.parseColor("#FF0000"));
+            return;
+        }
+
+        // all is correct
+        message.setVisibility(View.VISIBLE);
+        message.setText("The registration was successful!");
+        message.setTextColor(android.graphics.Color.parseColor("#42b72a"));
     }
 
-    private void validatePassword() {
-        EditText passwordEditText = findViewById(R.id.editTextTextPassword);
-        TextView errorTextView = findViewById(R.id.textViewPasswordError);
+    public String getImageFormat(Context context, Uri uri) {
+        ContentResolver contentResolver = context.getContentResolver();
 
-        String password = passwordEditText.getText().toString();
-        // Updated pattern to include at least one digit, one lowercase letter, one uppercase letter, and one special character
-        Pattern pattern = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\|,.<>\\/?]).{8,}$");
-        Matcher matcher = pattern.matcher(password);
-
-        if (password.isEmpty()) {
-            errorTextView.setText("Password cannot be empty");
-        } else if (password.length() < 8) {
-            errorTextView.setText("Password must be at least 8 characters long");
-        } else if (!matcher.matches()) {
-            errorTextView.setText("Password must contain at least one digit, one lowercase letter, one uppercase letter, and one special character");
-        } else {
-            errorTextView.setText("Good Password"); // Indicate the password is strong/correct
-            // Add your signup logic here
+        // Try to get the MIME type directly from the ContentResolver
+        String mimeType = contentResolver.getType(uri);
+        if (mimeType != null) {
+            return MimeTypeMap.getSingleton().getExtensionFromMimeType(mimeType);
         }
+
+        // If MIME type is not available, try to extract file extension from URI
+        String path = uri.getPath();
+        if (path != null) {
+            int lastDotIndex = path.lastIndexOf(".");
+            if (lastDotIndex != -1) {
+                return path.substring(lastDotIndex + 1);
+            }
+        }
+
+        // If all else fails, return null or a default value based on your requirements
+        return null;
     }
 
 
@@ -118,16 +186,26 @@ public class SignUp extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
 
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri imageUri = data.getData();
+           Uri image = data.getData();
+            if(!Objects.equals(getImageFormat(this, image), "jpeg") &&
+                    !Objects.equals(getImageFormat(this, image), "png") &&
+                    !Objects.equals(getImageFormat(this, image), "jpg"))
+                return;
+            TextView message = findViewById(R.id.text_sign_up);
+            message.setVisibility(View.GONE);
             ImageView profileImageView = findViewById(R.id.profileImageView);
-            profileImageView.setImageURI(imageUri);
+            profileImageView.setImageURI(image);
+            this.imageTaken = true;
+            profileImageView.setVisibility(View.VISIBLE);
         } else if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK && data != null) {
+            this.imageTaken = false;
             // Handle the camera result here, e.g., set the captured image to the ImageView
             Bundle extras = data.getExtras();
             if (extras != null) {
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 ImageView profileImageView = findViewById(R.id.profileImageView);
                 profileImageView.setImageBitmap(imageBitmap);
+                profileImageView.setVisibility(View.VISIBLE);
             }
         }
     }
