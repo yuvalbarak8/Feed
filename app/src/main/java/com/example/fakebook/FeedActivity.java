4b8a1b8969e618b6e965f229f6dde48a6187f0a2
itamar3188 <itamarc946@gmail.com>
2024-02-15 13:32:42 +0200

    style push

--- app/src/main/java/com/example/fakebook/FeedActivity.java
+++ app/src/main/java/com/example/fakebook/FeedActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
